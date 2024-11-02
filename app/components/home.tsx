e47c2fb573d68404178a6a94edcae0d3f5a9c0b5
SirRango <sturdivantdevon78@gmail.com>
2024-11-01 19:48:40 -0500

    added home page component

---
+++ app/components/home.tsx
@@ -0,0 +1,49 @@
+function Home() {
+  return (
+    <div className="flex flex-col p-5 pt-20 w-full">
+      <div className="flex flex-col items-center">
+        <h1 className="text-white text-[35px] font-semibold">
+          Bring your desired job market to you
+        </h1>
+        <h2 className="text-white text-[30px] font-semibold text-center">
+          And track Jobs you've applied to
+        </h2>
+        <p className="mt-5 text-white">
+          The new standard for applying and tracking jobs find your dream career
+          within minuets
+        </p>
+        <p className="text-white">
+          lorum ipsum so lador this is filler replace this with words later
+          dalor
+        </p>
+        <button className="mt-5 text-white p-2 font-medium w-[400px] rounded-md bg-[#FF6500]">
+          Start for Free
+        </button>
+        <p className="text-[15px] mt-2 text-[#fbfbfb65]">
+          14-day trial, no card required. start in minuets
+        </p>
+      </div>
+      <div className="w-full flex gap-10 items-center justify-center mt-28">
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+      </div>
+      <div className="w-full p-12">
+        <h2 className="text-white text-[26px]">
+          Search for in demand jobs in your area
+        </h2>
+        <h2 className="text-white text-[26px] pl-4">
+          look for the jobs your interested in
+        </h2>
+        <div className="w-full max-w-[400px] h-[230px] mt-16 rounded-lg bg-[#414141]"></div>
+        <div className="w-full max-w-[400px] h-[230px] mt-16 rounded-lg bg-[#414141]"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
