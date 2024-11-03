f0cf20968c9b0e6aaaec10a043ce9bb97e992fee
SirRango <sturdivantdevon78@gmail.com>
2024-11-02 19:16:51 -0500

    improved home page & added component from lib

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -1,3 +1,5 @@
+import { InfiniteMovingCards } from "./ui/MarqueeCards";
+
 function Home() {
   return (
     <div className="flex flex-col p-5 pt-20 w-full">
@@ -23,14 +25,12 @@ function Home() {
           14-day trial, no card required. start in minuets
         </p>
       </div>
-      <div className="w-full flex gap-10 items-center justify-center mt-28">
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
-        <div className="w-full max-w-48 h-72 rounded-lg bg-white"></div>
+      <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-[#141414] dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="left"
+          speed="slow"
+        />
       </div>
       <div className="w-full p-12">
         <h2 className="text-white text-[26px]">
@@ -45,5 +45,36 @@ function Home() {
     </div>
   );
 }
+// the content for the moving cards
+const testimonials = [
+  {
+    quote: "I found my dream job from this 10/10",
+    name: "Lebron James",
+    title: "Dream Job!",
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "William Shakespeare",
+    title: "Hamlet",
+  },
+  {
+    quote: "All that we see or seem is but a dream within a dream.",
+    name: "Edgar Allan Poe",
+    title: "A Dream Within a Dream",
+  },
+  {
+    quote:
+      "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+    name: "Jane Austen",
+    title: "Pride and Prejudice",
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "Moby-Dick",
+  },
+];
 
 export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,12 +8,52 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }: any) {
+  const colors = theme("colors");
+
+  const flattenColors = (
+    obj: Record<string, string | Record<string, string>>,
+    prefix = ""
+  ): Record<string, string> =>
+    Object.entries(obj).reduce((acc, [key, val]) => {
+      const newKey = prefix ? `${prefix}-${key}` : key;
+      if (typeof val === "string") {
+        acc[newKey] = val;
+      } else {
+        Object.assign(acc, flattenColors(val, newKey));
+      }
+      return acc;
+    }, {} as Record<string, string>);
+
+  const allColors = flattenColors(colors);
+
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default config;
