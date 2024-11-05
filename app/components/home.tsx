6398af201722882c884177fe85b73063f283e135
SirRango <sturdivantdevon78@gmail.com>
2024-11-05 14:37:09 -0600

    improved home page

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -1,49 +1,21 @@
 import { InfiniteMovingCards } from "./ui/MarqueeCards";
+import HpTitle from "./ui/HpTitle";
+import TextCard from "./ui/TextCard";
 
 function Home() {
   return (
-    <div className="flex flex-col p-5 pt-20 w-full dark:bg-[#1A1A19] bg-white dark:bg-grid-white/[0.1] bg-grid-black/[0.1] ">
-      <div className="flex flex-col items-center mt-12">
-        <h1 className="text-white text-[48px] font-semibold">
-          Bring the job market to you
-        </h1>
-        <h2 className="text-white text-[42px] font-semibold text-center">
-          Track every application effortlessly
-        </h2>
-        <p className="mt-5 text-white">
-          The new standard for applying and tracking jobs find your dream career
-          within minuets
-        </p>
-        <p className="text-white">
-          lorum ipsum so lador this is filler replace this with words later
-          dalor
-        </p>
-        <button
-          className="mt-5 text-[#F6FCDF] p-2 font-medium w-[400px] rounded-md "
-          style={{
-            background: "linear-gradient(90deg, #31511E, #859F3D",
-          }}
-        >
-          Start for Free
-        </button>
-        <p className="text-[15px] mt-2 text-[#fbfbfb65]">
-          14-day trial, no card required. start in minuets
-        </p>
-      </div>
-      <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-transparent  items-center justify-center relative overflow-hidden">
+    <div className="flex flex-col p-5 pt-20 w-full dark:bg-[#1A1A19] bg-white ">
+      <HpTitle />
+      <div className="h-[20rem] rounded-md flex flex-col antialiased bg-white dark:bg-transparent  items-center justify-center relative overflow-hidden">
         <InfiniteMovingCards
           items={testimonials}
           direction="left"
-          speed="slow"
+          speed="normal"
         />
       </div>
-      <div className="w-full p-12">
-        <h2 className="text-white text-[26px]">
-          Search for in demand jobs in your area
-        </h2>
-        <h2 className="text-white text-[26px] pl-4">
-          look for the jobs your interested in
-        </h2>
+      <div className="flex flex-col w-full p-12 gap-60">
+        <TextCard />
+        <TextCard />
       </div>
     </div>
   );
