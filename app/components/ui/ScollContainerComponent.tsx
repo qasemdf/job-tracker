1f4c870824d7cfcf054ee39beeebdfd2d8fbaa44
SirRango <sturdivantdevon78@gmail.com>
2024-11-11 08:26:48 -0600

    added scroll component

---
+++ app/components/ui/ScollContainerComponent.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { ContainerScroll } from "./ContainerScrollAnimation";
+import Image from "next/image";
+
+export function ScrollContainerComponent() {
+  return (
+    <div className="flex flex-col overflow-hidden h-[850px]">
+      <ContainerScroll titleComponent={<></>}>
+        <Image
+          src={`/linear.webp`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
+}
