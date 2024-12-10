437699195ef7a27ca54dc5ba60534e52ea5115ad
SirRango <sturdivantdevon78@gmail.com>
2024-12-10 09:34:39 -0600

    refactored TextCard to use props

--- app/components/ui/TextCard.tsx
+++ app/components/ui/TextCard.tsx
@@ -1,21 +1,19 @@
 import { quicksand } from "@/app/fonts/Fonts";
 import { staatliches } from "@/app/fonts/Fonts";
 
-function TextCard() {
+function TextCard(props) {
   return (
     <div className="flex w-full items-center justify-center gap-16 ">
       <div className="max-w-[550px]">
         <h1
           className={` ${staatliches.className} text-[50px] font-semibold text-wrap text-[#ECDFCC]`}
         >
-          Lorem ipsum dolor sit amet, consectetur adipisicing
+          {props.title}
         </h1>
         <p
           className={` ${quicksand.className} mt-10 text-[17px] text-[#fbfbfb65]`}
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sequi
-          optio accusantium, voluptate magnam odit quibusdam incidunt possimus,
-          rem dolorum
+          {props.description}
         </p>
       </div>
       <div className="w-full max-w-[750px] h-[490px] bg-white rounded-2xl"></div>
