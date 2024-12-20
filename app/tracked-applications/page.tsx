147ae65a438b1314ab00d53921c9555b88ced25b
deanqasem <deanqasem@gmail.com>
2024-12-19 23:31:43 -0500

    what happened

--- app/tracked-applications/page.tsx
+++ app/tracked-applications/page.tsx
@@ -11,6 +11,7 @@ import {
   query,
   getDocs,
   addDoc,
+  updateDoc,
 } from "firebase/firestore";
 
 interface JobApplication {
@@ -31,6 +32,8 @@ const TrackedApplicationsPage: React.FC = () => {
     applicationDate: "",
     description: "",
   });
+  const [editingApplication, setEditingApplication] =
+    useState<JobApplication | null>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -70,7 +73,14 @@ const TrackedApplicationsPage: React.FC = () => {
   }, [router]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewApplication({ ...newApplication, [e.target.name]: e.target.value });
+    if (editingApplication) {
+      setEditingApplication({
+        ...editingApplication,
+        [e.target.name]: e.target.value,
+      });
+    } else {
+      setNewApplication({ ...newApplication, [e.target.name]: e.target.value });
+    }
   };
 
   const handleAddApplication = async () => {
@@ -108,6 +118,7 @@ const TrackedApplicationsPage: React.FC = () => {
       console.error("Error adding application:", error);
     }
   };
+
   const handleDeleteApplication = async (id: string) => {
     const confirmed = window.confirm("Are you sure you want to delete?");
     if (confirmed) {
@@ -130,8 +141,37 @@ const TrackedApplicationsPage: React.FC = () => {
     }
   };
 
+  const handleEditApplication = (application: JobApplication) => {
+    setEditingApplication(application);
+  };
+
+  const handleUpdateApplication = async () => {
+    if (!editingApplication) return;
+
+    try {
+      const applicationDoc = doc(
+        firestore,
+        "users",
+        user.uid,
+        "applications",
+        editingApplication.id
+      );
+      await updateDoc(applicationDoc, editingApplication);
+
+      setApplications((prev) =>
+        prev.map((app) =>
+          app.id === editingApplication.id ? editingApplication : app
+        )
+      );
+
+      setEditingApplication(null);
+    } catch (error) {
+      console.error("Error updating application:", error);
+    }
+  };
+
   if (loading) {
-    return <p>Loading...</p>;
+    return <p className="text-center text-lg mt-10">Loading...</p>;
   }
 
   return (
@@ -165,7 +205,14 @@ const TrackedApplicationsPage: React.FC = () => {
                 >
                   Delete
                 </button>
+<<<<<<< Updated upstream
                 <button className="w-32 h-12 dark:bg-[#697565] bg-[#D8C4B6] dark:text-[#ECDFCC] text-[#F5EFE7] rounded-lg font-medium hover:bg-[#d8c4b6b4] focus:ring focus:ring-[#1b1b1b]">
+=======
+                <button
+                  onClick={() => handleEditApplication(app)}
+                  className="w-32 h-12 dark:bg-[#697565] bg-[#D8C4B6] dark:text-[#ECDFCC] text-[#F5EFE7] rounded-lg font-medium hover:bg-[#d8c4b6b4] focus:ring focus:ring-[#1b1b1b]"
+                >
+>>>>>>> Stashed changes
                   Edit
                 </button>
               </div>
@@ -174,13 +221,21 @@ const TrackedApplicationsPage: React.FC = () => {
         </div>
         <div className="container flex flex-col justify-center items-center">
           <h2 className="text-2xl font-bold mb-4 dark:text-[#ECDFCC] text-[#F5EFE7]">
+<<<<<<< Updated upstream
             Add New Application
+=======
+            {editingApplication ? "Edit Application" : "Add New Application"}
+>>>>>>> Stashed changes
           </h2>
           <div className="w-full max-w-[700px] mb-4">
             <input
               type="text"
               name="companyName"
-              value={newApplication.companyName}
+              value={
+                editingApplication
+                  ? editingApplication.companyName
+                  : newApplication.companyName
+              }
               onChange={handleInputChange}
               placeholder="Company Name"
               className="w-full max-w-[700px] p-2 border rounded dark:bg-[#3C3D37] bg-[#3E5879] dark:text-[#ECDFCC] text-[#F5EFE7] dark:placeholder-[#ECDFCC] placeholder-[#F5EFE7]"
@@ -190,7 +245,11 @@ const TrackedApplicationsPage: React.FC = () => {
             <input
               type="text"
               name="position"
-              value={newApplication.position}
+              value={
+                editingApplication
+                  ? editingApplication.position
+                  : newApplication.position
+              }
               onChange={handleInputChange}
               placeholder="Position"
               className="w-full max-w-[700px]  p-2 border rounded dark:bg-[#3C3D37] bg-[#3E5879] dark:text-[#ECDFCC] text-[#F5EFE7] dark:placeholder-[#ECDFCC] placeholder-[#F5EFE7]"
@@ -200,7 +259,11 @@ const TrackedApplicationsPage: React.FC = () => {
             <input
               type="text"
               name="description"
-              value={newApplication.description}
+              value={
+                editingApplication
+                  ? editingApplication.description
+                  : newApplication.description
+              }
               onChange={handleInputChange}
               placeholder="Description"
               className="w-full max-w-[700px]  p-2 border rounded dark:bg-[#3C3D37] bg-[#3E5879] dark:text-[#ECDFCC] text-[#F5EFE7] dark:placeholder-[#ECDFCC] placeholder-[#F5EFE7]"
@@ -210,18 +273,41 @@ const TrackedApplicationsPage: React.FC = () => {
             <input
               type="date"
               name="applicationDate"
-              value={newApplication.applicationDate}
+              value={
+                editingApplication
+                  ? editingApplication.applicationDate
+                  : newApplication.applicationDate
+              }
               onChange={handleInputChange}
               placeholder="Application Date"
               className="w-full max-w-[700px]  p-2 border rounded dark:bg-[#3C3D37] bg-[#3E5879] dark:text-[#ECDFCC] text-[#F5EFE7] dark:placeholder-[#ECDFCC]"
             />
           </div>
+<<<<<<< Updated upstream
           <button
             onClick={handleAddApplication}
             className="px-4 py-2 mb-7 dark:bg-[#697565] bg-[#D8C4B6] text-[#F5EFE7] dark:text-[#ECDFCC] rounded hover:bg-[#69756598]"
           >
             Add Application
           </button>
+=======
+          {editingApplication ? (
+            <button
+              onClick={handleUpdateApplication}
+              className="px-4 py-2 mb-7 dark:bg-[#697565] bg-[#D8C4B6] text-[#F5EFE7] dark:text-[#ECDFCC] rounded hover:bg-[#69756598]"
+            >
+              {" "}
+              Update Application
+            </button>
+          ) : (
+            <button
+              onClick={handleAddApplication}
+              className="px-4 py-2 mb-7 dark:bg-[#697565] bg-[#D8C4B6] text-[#F5EFE7] dark:text-[#ECDFCC] rounded hover:bg-[#69756598]"
+            >
+              Add Application
+            </button>
+          )}
+>>>>>>> Stashed changes
         </div>
       </main>
     </div>
